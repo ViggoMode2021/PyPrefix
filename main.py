7bfaed1035f954c1245a8064497237d5c1ff6551
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2025-02-28 19:16:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
- PyPrefix - IP Prefix-List checker for Cisco networking devices
+# PyPrefix - IP Prefix-List checker for Cisco networking devices
 
 # Author: Ryan Viglione
 
-from netaddr import IPNetwork
 import ipaddress
+from netaddr import IPNetwork
 import pyfiglet
+import cymruwhois
+import socket
+from cymruwhois import Client
 
 def prefix_list_checker():
+
     PyPrefix_title = pyfiglet.figlet_format("PyPrefix")
     print(PyPrefix_title)
 
@@ -14,53 +18,67 @@ def prefix_list_checker():
 
     while True:
         subnet = input(
-            "ip prefix-list TEST permit ______________ ge __ le __")  # user specifies supernet to validate prefixes against
+            "ip prefix-list TEST permit ______________ ge __ le __\n")  # user specifies supernet to validate prefixes against
 
         try:
             ipaddress.ip_network(subnet)  # validate supernet formatting to be a prefix with subnet expressed in CIDR
             break  # break loop if above condition is met
         except ValueError:  # throw exception if prefix is not in prefix + CIDR format (example - 192.168.10.0/24)
-            print(f"{subnet} is not a valid subnet with CIDR notation.")
+            print(f"\n{subnet} is not a valid subnet with CIDR notation.\n")
 
     while True:
 
-        print(" ~~~~ Input a greater than mask length. ~~~~\n")
+        socket_client = Client()
+
+        prefix_network = IPNetwork(subnet)
+
+        broadcast_of_prefix_network = prefix_network.broadcast
+
+        broadcast_of_prefix_network_search = socket_client.lookup(broadcast_of_prefix_network)
+
+        prefix_asn = broadcast_of_prefix_network_search.asn
+
+        prefix_owner = broadcast_of_prefix_network_search.owner
+
+        print(f"\nThe prefix {subnet} belongs to Autonomous System # {prefix_asn} {prefix_owner}")
+
+        print("\n ~~~~ Input a greater than mask length. ~~~~\n")
 
         greater_than_mask = (
-            input(f"ip prefix-list TEST permit {subnet} ge __ le __"))  # user specifies greater than mask
+            input(f"\nip prefix-list TEST permit {subnet} ge __ le __"))  # user specifies greater than mask
 
         try:
             greater_than_mask = int(greater_than_mask)
             if 0 <= greater_than_mask <= 32:
                 break
             elif greater_than_mask > 32:
-                print(f"Input {greater_than_mask} is out of valid range.")
+                print(f"\nInput {greater_than_mask} is out of valid range.")
         except ValueError:
-            print(f"{greater_than_mask} is not a valid integer!")
+            print(f"\n{greater_than_mask} is not a valid integer!")
 
     while True:
 
         print(" ~~~~ Input a less than mask length. ~~~~\n")
 
-        less_than_mask = int(input(f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le __"))
+        less_than_mask = int(input(f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le __\n"))
 
         try:
             less_than_mask = int(less_than_mask)
             if 0 <= less_than_mask <= 32:
                 break
             elif less_than_mask > 32:
-                print(f"Input {less_than_mask} is out of valid range.")
+                print(f"Input {less_than_mask} is out of valid range.\n")
         except ValueError:
-            print(f"{less_than_mask} is not a valid integer!")
+            print(f"{less_than_mask} is not a valid integer!\n")
 
-    full_statement = f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le {less_than_mask}"
+    full_statement = f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le {less_than_mask}\n"
 
     print(full_statement)
 
     subnet_lists = []
 
     while True:
-        subnet_inputs = input(f"Add prefixes to check against {full_statement}.")
+        subnet_inputs = input(f"Add prefixes to check against {full_statement}\n.")
 
         if subnet_inputs == "done":
             break
@@ -68,20 +86,35 @@ def prefix_list_checker():
         try:
             ipaddress.ip_network(subnet_inputs)
             subnet_lists.append(subnet_inputs)
+
+            socket_client = Client()
+
+            prefix_network = IPNetwork(subnet_inputs)
+
+            broadcast_of_prefix_network = prefix_network.broadcast
+
+            broadcast_of_prefix_network_search = socket_client.lookup(broadcast_of_prefix_network)
+
+            prefix_asn = broadcast_of_prefix_network_search.asn
+
+            prefix_owner = broadcast_of_prefix_network_search.owner
+
+            print(f"The prefix {subnet_inputs} belongs to Autonomous System # {prefix_asn} {prefix_owner}\n")
+
         except ValueError:
-            print(f"{subnet_inputs} is not a valid subnet with CIDR notation.")
+            print(f"{subnet_inputs} is not a valid subnet with CIDR notation.\n")
 
         for network in subnet_lists:
             mask = int(network.split("/", 1)[1])
 
             if IPNetwork(network) in IPNetwork(subnet) and greater_than_mask <= mask <= less_than_mask:
-                print(f"Yes, {network} is in {subnet} and meets the criteria of {full_statement}")
+                print(f"Yes, {network} is in {subnet} and meets the criteria of {full_statement}\n")
                 subnet_lists.remove(subnet_inputs)
             elif IPNetwork(network) in IPNetwork(subnet):
                 print(
-                    print(f"The {network} network is in {subnet}, but doesn't meet the comparison operator criteria."))
+                    print(f"The {network} network is in {subnet}, but doesn't meet the comparison operator criteria.\n"))
             else:
-                print(f"No, {network} does not meet the criteria of {full_statement}.")
+                print(f"No, {network} does not meet the criteria of {full_statement}\n.")
 
 if __name__ == "__main__":
     prefix_list_checker()
